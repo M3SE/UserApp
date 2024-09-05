12b0d0a76d071e48f63aead02cb10e5031295631
M3SE <simonc691@gmail.com>
2024-09-05 16:22:22 -0400

    First Commit

---
+++ src/test/java/com/example/usermanagementapp2/UserManagementApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermanagementapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
